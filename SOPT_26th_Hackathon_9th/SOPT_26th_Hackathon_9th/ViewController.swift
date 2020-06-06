9a290ea0567ef671dce92a16e36455176267ea25
hansolnoh95 <hansolnoh95@naver.com>
2020-06-07 01:56:45 +0900

    create project

---
+++ SOPT_26th_Hackathon_9th/SOPT_26th_Hackathon_9th/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SOPT_26th_Hackathon_9th
+//
+//  Created by 노한솔 on 2020/06/07.
+//  Copyright © 2020 노한솔. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
